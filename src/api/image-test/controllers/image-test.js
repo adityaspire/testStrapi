b0bb4050f7c147884752fd7391c99be790cc96ac
aditya.shrivastav <aditya.shrivastav>
2022-08-24 21:09:30 +0530

    test comnmit

---
+++ src/api/image-test/controllers/image-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  image-test controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::image-test.image-test');
---
+++ src/api/image-test/routes/image-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-test router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::image-test.image-test');
